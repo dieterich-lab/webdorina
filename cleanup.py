b65c49a1c2de890cc28182cac3b9ed02f3e28b57
Kai Blin <kai.blin@age.mpg.de>
2014-08-29 11:50:05 +0200

    cleanup: Add a daemon process to clean up session dirs
    Signed-off-by: Kai Blin <kai.blin@age.mpg.de>

---
+++ cleanup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# :set fileencoding=utf-8 :
+
+import shutil
+import daemon
+from os import path
+from redis import StrictRedis
+
+def callback(message):
+    if message['data'].startswith('sessions:'):
+        uuid = message['data'].split(':')[-1]
+        session_dir = path.join('/tmp', "dorina-{}".format(uuid))
+        print "deleting {session_dir}".format(session_dir=session_dir)
+        if path.exists(session_dir):
+            shutil.rmtree(session_dir, ignore_errors=True)
+
+def main():
+    r = StrictRedis()
+    r.config_set('notify-keyspace-events', 'Ex')
+    p = r.pubsub(ignore_subscribe_messages=True)
+    p.psubscribe(**{'__keyevent@0__:expired': callback})
+    # pop the subscribe message
+    p.get_message()
+    for message in p.listen():
+        pass
+
+
+if __name__ == "__main__":
+    with daemon.DaemonContext():
+        main()
