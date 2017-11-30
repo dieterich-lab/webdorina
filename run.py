45cd994ead33feec245f762de54a979b31e9a869
Thiago Britto Borges <tbrittoborges@dundee.ac.uk>
2017-11-30 15:59:02 +0100

    py2 > py3 change line wrapping configure Redis to use unicode

--- run.py
+++ run.py
@@ -1,20 +1,29 @@
-import json
-from redis import Redis
-import time
-from dorina import run
+#!/usr/bin/env python
+# coding=utf-8
 
-def run_analyse(datadir, query_key, query_pending_key, query, uuid, timeit=False):
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import json
+import time
+
+from dorina import run
+from redis import Redis
+
+
+def run_analyse(datadir, query_key, query_pending_key, query, uuid,
+                timeit=False):
     dorina = run.Dorina(datadir)
 
     from webdorina import RESULT_TTL, SESSION_TTL, SESSION_STORE
-    redis_store = Redis()
+    redis_store = Redis(charset="utf-8", decode_responses=True)
 
     if timeit:
         started = time.time()
 
     session_store = SESSION_STORE.format(unique_id=uuid)
     custom_regulator_file = '{session_store}/{uuid}.bed'.format(
-            session_store=session_store, uuid=uuid)
+        session_store=session_store, uuid=uuid)
     set_a = []
     for regulator in query['set_a']:
         if regulator == uuid:
@@ -34,20 +43,21 @@ def run_analyse(datadir, query_key, query_pending_key, query, uuid, timeit=False
     try:
         result = str(dorina.analyse(**query))
     except Exception as e:
-        result = '\t\t\t\t\t\t\t\tJob failed: %s' % str(e).replace('\n', ' ').replace('\t', ' ')
+        result = '\t\t\t\t\t\t\t\tJob failed: %s' % str(e).replace(
+            '\n', ' ').replace('\t', ' ')
 
     if timeit:
         analysed = time.time()
-        print "analyse() ran {} seconds".format(analysed - started)
+        print("analyse() ran {} seconds".format(analysed - started))
 
     lines = result.split('\n')
     if lines[-1] == '':
         lines = lines[:-1]
 
     def get_score(x):
-	cols = x.split('\t')
-	if len(cols) < 14:
-	    return -1
+        cols = x.split('\t')
+        if len(cols) < 14:
+            return -1
         try:
             return float(cols[13])
         except ValueError:
@@ -57,35 +67,37 @@ def run_analyse(datadir, query_key, query_pending_key, query, uuid, timeit=False
 
     if timeit:
         sorted_time = time.time()
-        print "sort() ran {} seconds".format(sorted_time - analysed)
+        print("sort() ran {} seconds".format(sorted_time - analysed))
 
     num_results = len(lines)
-    print "returning %s rows" % num_results
+    print("returning %s rows" % num_results)
 
     if num_results == 0:
         lines = ['\t\t\t\t\t\t\t\tNo results found']
         num_results += 1
 
-    for i in xrange(0, num_results, 1000):
-        res = lines[i:i+1000]
+    for i in range(0, num_results, 1000):
+        res = lines[i:i + 1000]
         redis_store.rpush(query_key, *res)
 
-
     redis_store.expire(query_key, RESULT_TTL)
-    redis_store.setex('sessions:{0}'.format(uuid), json.dumps(dict(state='done', uuid=uuid)), SESSION_TTL)
-    redis_store.setex('results:sessions:{0}'.format(uuid), json.dumps(dict(redirect=query_key)), SESSION_TTL)
+    redis_store.setex('sessions:{0}'.format(uuid), json.dumps(dict(
+        state='done', uuid=uuid)), SESSION_TTL)
+    redis_store.setex('results:sessions:{0}'.format(uuid), json.dumps(dict(
+        redirect=query_key)), SESSION_TTL)
 
     redis_store.delete(query_pending_key)
     if timeit:
+        json_time = 0  # I can't find this variable in the project
         pushed = time.time()
-        print "push() ran {} seconds".format(pushed - json_time)
-        print "total time taken: {} seconds".format(pushed - started)
+        print("push() ran {} seconds".format(pushed - json_time))
+        print("total time taken: {} seconds".format(pushed - started))
 
 
 def filter(genes, full_query_key, query_key, query_pending_key, uuid):
     """Filter for a given set of gene names"""
     from webdorina import RESULT_TTL, SESSION_TTL
-    redis_store = Redis()
+    redis_store = Redis(charset="utf-8", decode_responses=True)
 
     full_results = redis_store.lrange(full_query_key, 0, -1)
     results = []
@@ -104,12 +116,14 @@ def filter(genes, full_query_key, query_key, query_pending_key, uuid):
         results.append('\t\t\t\t\t\t\t\tNo results found')
         num_results += 1
 
-    for i in xrange(0, num_results, 1000):
-        res = results[i:i+1000]
+    for i in range(0, num_results, 1000):
+        res = results[i:i + 1000]
         redis_store.rpush(query_key, *res)
 
     redis_store.expire(query_key, RESULT_TTL)
     redis_store.delete(query_pending_key)
 
-    redis_store.setex('sessions:{0}'.format(uuid), json.dumps(dict(state='done', uuid=uuid)), SESSION_TTL)
-    redis_store.setex('results:sessions:{0}'.format(uuid), json.dumps(dict(redirect=query_key)), SESSION_TTL)
+    redis_store.setex('sessions:{0}'.format(uuid), json.dumps(dict(
+        state='done', uuid=uuid)), SESSION_TTL)
+    redis_store.setex('results:sessions:{0}'.format(uuid), json.dumps(dict(
+        redirect=query_key)), SESSION_TTL)
