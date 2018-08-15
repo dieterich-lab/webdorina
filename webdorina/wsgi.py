3e829a399b916e0dcd23d9c5c112eeba05d8d7ba
Thiago Britto Borges <tbrittoborges@dundee.ac.uk>
2018-08-15 10:42:46 +0200

    Better logging at the devel server

--- webdorina/wsgi.py
+++ webdorina/wsgi.py
@@ -8,7 +8,7 @@ import site
 # The path of the webdorina project directory is the same as the directory of this file
 PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-logging.basicConfig(stream=sys.stderr)
+logging.basicConfig(level=logging.DEBUG)
 
 site.addsitedir(PROJECT_DIR)
 sys.path.append(PROJECT_DIR)
